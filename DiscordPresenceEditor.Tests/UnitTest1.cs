fcf1cc0f539510bb49f2288ad65bede28ca45bd6
fasetto <serkanbircan21@yandex.com>
2020-02-06 01:20:47 +0300

    initial commit

---
+++ DiscordPresenceEditor.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DiscordPresenceEditor.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
