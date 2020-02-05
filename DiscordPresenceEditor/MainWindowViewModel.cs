a4d44495d1c6f5d9095180fb089ae3160979ff6a
fasetto <serkanbircan21@yandex.com>
2020-02-06 01:22:23 +0300

    version 0.1.1

---
+++ DiscordPresenceEditor/MainWindowViewModel.cs
@@ -0,0 +1,99 @@
+﻿using DiscordPresenceEditor.Commands;
+using DiscordPresenceEditor.Models;
+using DiscordRPC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DiscordPresenceEditor
+{
+    public class MainWindowViewModel: INotifyPropertyChanged
+    {
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set
+            {
+                _statusMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand UpdatePresenceCommand => new RelayCommand(x => UpdatePresence(), p => CanUpdatePresence());
+
+        public MainWindowViewModel()
+        {
+            PresenceConfig = new Config(Client);
+            ReadConfig();
+        }
+
+        public DiscordRpcClient Client { get; set; }
+        public Config PresenceConfig { get; set; }
+
+        public async void UpdatePresence()
+        {
+            var presence = new RichPresence()
+            {
+                Details = PresenceConfig.Details,
+                State = PresenceConfig.State,
+                Assets = new Assets()
+                {
+                    LargeImageKey = PresenceConfig.ImageKey,
+                    LargeImageText = PresenceConfig.ImageText,
+                }
+            };
+            
+            if (PresenceConfig.ShowElapsedTime)
+                presence.Timestamps = new Timestamps(DateTime.UtcNow);
+
+            Client.SetPresence(presence);
+
+            StatusMessage = "Your discord presence updated successfully..";
+
+            SaveConfig();
+
+            await Task.Delay(5000);
+
+            StatusMessage = "";
+        }
+        private void ReadConfig()
+        {
+            if (!File.Exists("./config.json"))
+                return;
+
+            var json = File.ReadAllText("./config.json");
+
+            PresenceConfig = JsonConvert.DeserializeObject<Config>(json);
+
+            Client = new DiscordRpcClient(PresenceConfig.ClientId);
+            Client.Initialize();
+        }
+
+        private void SaveConfig()
+        {
+
+            var json = JsonConvert.SerializeObject(PresenceConfig, Formatting.Indented);
+            File.WriteAllText("./config.json", json);
+        }
+
+        public bool CanUpdatePresence()
+        {
+            var canUpdate = !string.IsNullOrWhiteSpace(PresenceConfig.ClientId);
+            return canUpdate;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ DiscordPresenceEditor/Models/Config.cs
@@ -0,0 +1,98 @@
+﻿using DiscordRPC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordPresenceEditor.Models
+{
+    public class Config: INotifyPropertyChanged
+    {
+        private string _clientId;
+        public string ClientId
+        {
+            get => _clientId;
+            set
+            {
+                _clientId = value;
+                OnPropertyChanged();
+
+                _client = new DiscordRpcClient(_clientId);
+                _client.Initialize();
+            }
+        }
+
+        private string _state;
+        public string State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _details;
+        public string Details
+        {
+            get => _details;
+            set
+            {
+                _details = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _imageKey;
+        public string ImageKey
+        {
+            get => _imageKey;
+            set
+            {
+                _imageKey = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _imageText;
+        public string ImageText
+        {
+            get => _imageText;
+            set
+            {
+                _imageText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _showElapsedTime;
+        public bool ShowElapsedTime
+        {
+            get => _showElapsedTime;
+            set
+            {
+                _showElapsedTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DiscordRpcClient _client;
+
+        public Config(DiscordRpcClient client)
+        {
+            _client = client;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
